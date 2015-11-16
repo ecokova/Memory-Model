0282da709d10169e9d77b56f299cc30364c5fbea
njzarrilli <njzarrilli@gmail.com>
2015-11-16 15:50:51 -0500

    Incorporated flexqueue into implementation

--- memory.py
+++ memory.py
@@ -7,19 +7,34 @@ Edited 11/9/15
 
 import threading
 import time
+import FlexQueue 
 
 
 class MemoryThread(threading.Thread):
-    def __init__(self, shared_mutex, max_size, DONE):
-        threading.Thread = __init__(self)
-        self.queue = Queue(max_size)
+    def __init__(self, shared_mutex, DONE, queue, wait_time):
+        threading.Thread = super(MemoryThread, self).__init__()
+        self.queue = queue
         self.mutex = shared_mutex
         self.DONE = DONE
+        self.wait_time = wait_time
     def run(self):
         start_time = time.time()
+        print start_time
         while not self.DONE:
-            if ((time.time - start_time) % 5.00 == 0):
+            if ((time.time() - start_time) % self.wait_time == 0):
                 try:
-                    self.queue.get()
-                except Empty:
+                    x = self.queue.get()
+                    #print x
+                except self.queue.Empty:
                     pass
+
+#Testing
+mutex = threading.Lock()
+DONE = False
+wait = .2
+q = FlexQueue.FlexQueue(3)
+q.put(1)
+q.put(2)
+q.put(3)
+t = MemoryThread(mutex, DONE, q, wait)
+t.run()

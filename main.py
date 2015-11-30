62c976f78a72325f8924013922b76dae9373d5e2
Jen Hammelman <jennifer.hammelman@tufts.edu>
2015-11-30 16:13:24 -0500

    refine input

--- main.py
+++ main.py
@@ -13,26 +13,52 @@ import threading
 import sys
 from collections import namedtuple
 import time
+import argparse
 
 Association = namedtuple("MyStruct", "association frequency")
 
+def parseQs(qfile):
+    questions = []
+    results = []
+    with open qfile as f:
+        lines = [line for line in f]
+        for i in xrange(0,len(lines),5):
+            wc = lines[i:i+4]
+            questions.append("{}\n{}\n{}".format(wc[0],wc[1],wc[2]))
+            results.append({wc[3],wc[4]})
+    return questions, results
+
 def main(argv):
+    parser = argparse.ArgumentParser(description='MBTI Memory Model')
+    parser.add_argument('-q','numQs', help='number of questions')
+    parser.add_argument('-qfile', 'qfile', help='question file', 
+                        default='myers-briggs.txt')
+    parser.add_argument('-uinput', 'input', help='user input',
+                        default='stdin')
+    opts = parser.parse_args()
+    
     DONE = [False]
+    [questions, results] = parseQs(opts.qfile)
+
     LTM = memory.MemoryThread(DONE, 0.2, 0)
     STM = memory.MemoryThread(DONE, 0.2, 7)
     SM  = memory.MemoryThread(DONE, 0.2, 5)
+
     RHS = action.Action(5,STM, LTM, 0.2, DONE)
     ATN = action.Action(0, SM, STM, 0.1, DONE)
     RTV = retrieval.Retrieval(STM.queue, LTM.queue, DONE, threading.Lock(), 0.2)
+
     threads = [LTM, STM, SM, RHS, ATN, RTV]
     for thread in threads:
         thread.start()
-    with sys.stdin as f:
-        for line in f:
-            words = line.split()
-            SM.queue.put(Association(words,0))
-            time.sleep(0.2)
-    f.close()
+    for i in range(opts.numQs):
+        ans = input(questions[i])
+        if ans == 'A':
+            result = results[i]{0}
+        else:
+            result = results[i]{1}
+        SM.queue.put(Association(result,0))
+
     DONE[0] = True
     for thread in threads:
         thread.join()

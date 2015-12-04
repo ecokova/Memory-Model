1c1d78962fe685e18af542d1b61978917bf73b4f
Jennifer Hammelman <jhammelman@gmail.com>
2015-12-04 18:29:30 -0500

    working barchart!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 main.py
 Jen Hammelman, Naomi Zarrilli, and Elena Cokova
 Created 11/22/2015
-Edited 11/22/2015
+Edited 12/4/2015
 '''
 
 import memory
@@ -26,21 +26,29 @@ import numpy as np
 
 Association = namedtuple("MyStruct", "association frequency")
 
+traitNames = ['Extraversion','Introversion', \
+              'Sensing','iNtuition', 'Thinking','Feeling',
+              'Perceiving','Judging']
+colors = ['r','b', 'g','y', 'k','w','m','c']
+
 def analyzePersonality(memories):
-    all_traits = [('judging-function', 'Thinking', 'Feeling'), \
-            ('attitude', 'Extraversion', 'Introversion'), \
-            ('lifestyle', 'Perceiving', 'Judging'), \
-            ('perceiving-function', 'Sensing', 'iNtuition')]
+    allTraits = [('attitude', 'Extraversion', 'Introversion'), \
+                 ('perceiving-function', 'Sensing', 'iNtuition'), \
+                 ('judging-function', 'Thinking', 'Feeling'), \
+                 ('lifestyle', 'Perceiving', 'Judging')]
     MBTIlist= list()
-    for mem in memories: 
+
+    for i in range(len(memories)):
+        mem = memories[i]
         assoc = [x[0] for x in mem.queue.queue]
         words = '+'.join(assoc) #reduce(operator.add, stmAssoc, '')
-        
-        for trait in all_traits:
+        for trait in allTraits:
+            num = [0.5, 0.5]
             text = words.replace(' ', '+')
             text = text.replace('\n','')
+            
             url = 'http://uclassify.com/browse/prfekt/myers-briggs-' + trait[0] + '/' + \
-                    'ClassifyText?readkey=jHP7vqc5rX83&' + \
+                    'ClassifyText?readkey=lt42BVovxems' + \
                     'text=' + text + '&version=1.01&output=json'
             try:
                 result = urllib2.urlopen(url)
@@ -50,25 +58,24 @@ def analyzePersonality(memories):
             except urllib2.URLError, e:
                 print e
                 pass
+           
+            '''
+            MBTIlist.append(num[0])
+            MBTIlist.append(num[1])
+            '''
     return MBTIlist
 
-'''
- define a function that collects personality results into 
- some global variable
-
- call that function from main in a thread
-
- upon joining all threads, call the animation barplot over
- our collection of multiple personalities
-
-'''
-
-def animateBarplot(fig, rects, memories):
-    print "in animate barplot"
-    x = analyzePersonality(memories)
-    for rect,h in zip(rects,x):
-	rect.set_height(h)
-    fig.canvas.draw()
+def getPersonality(personalityResults, memories, DONE):
+    while not DONE[0]:
+        personalityResults.append(analyzePersonality(memories))
+        
+def animateBarplot(fig, rects, personalityResults):
+    half = len(rects)/2
+    for results in personalityResults:
+        time.sleep(0.3)
+        for i in xrange(0,len(rects)):
+            rects[i].set_height(results[i])
+        fig.canvas.draw()
 		
 def parseQs(qfile):
     questions = []
@@ -104,43 +111,51 @@ def main(argv):
     RHS = action.Action(5,STM, LTM, 0.2, DONE)
     ATN = action.Action(0, SM, STM, 0.1, DONE)
     RTV = retrieval.Retrieval(STM.queue, LTM.queue, DONE, threading.Lock(), 0.2)
-    x = analyzePersonality([STM, LTM])
+    personalityResults = []
+    
     threads = [LTM, STM, SM, RHS, ATN, RTV]
 
-    plt.ion()
-    fig = plt.figure()
-    win = fig.canvas.manager.window
-    rects = plt.bar(range(len(x)), x, align='center')
-    plt.show()
+    personalityAccumulator = threading.Thread(target =getPersonality,
+                                              args=[personalityResults, [STM, LTM], DONE])
+    personalityAccumulator.start()
 
     for thread in threads:
         thread.start()
 
-    win.after(100, lambda: animateBarplot(fig, rects, [STM, LTM]))
-
     for i in range(opts.numQs):
         ans = raw_input(questions[i])
         if ans == 'A':
-            
             result = results[i][0]
         else:
             result = results[i][1]
         SM.queue.put(Association(result,0))
 
     DONE[0] = True
+    
     for thread in threads:
         thread.join()
+    personalityAccumulator.join()
 
+    if personalityResults:
+        plt.ion()
+        fig = plt.figure()
+        win = fig.canvas.manager.window
+        x = personalityResults[0]
+        print x
+        rects = plt.bar(range(len(x)), x, align='center', color=colors)
+        plt.xticks(range(len(x)), traitNames+traitNames, rotation='vertical')
+        plt.show()
+        win.after(100, lambda: animateBarplot(fig, rects, personalityResults))
 
-
+    else:
+        print "We didn't get enough info to form your personality. Please try again!"
+    
     print "Long Term Memory: ", list(LTM.queue.queue)
     print
     print "Short Term Memory: ", list(STM.queue.queue)
     print
     print "Sensory Memory: ", list(SM.queue.queue)
-
-    print '++++++++++++++++++++++++++++++++++'
-    print analyzePersonality([STM, LTM])
+    
     done = raw_input("Done, ok?")
     
     exit(0)
